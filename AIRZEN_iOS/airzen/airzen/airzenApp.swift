928aa1b32aaad684f7f59ad462cb0e7fea9bf447
anfasssss <220730149+anfasssss@users.noreply.github.com>
2026-09-21 20:20:21 +0530

    feat(ios): Add 5 large sensor cards, prominent fire alert, 2 control cards, splash animation, and app icon

---
+++ AIRZEN_iOS/airzen/airzen/airzenApp.swift
@@ -0,0 +1,17 @@
+//
+//  airzenApp.swift
+//  airzen
+//
+//  Created by anfas on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct airzenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
